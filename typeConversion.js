97e23947a1e7f8f7b8687ebf7cab93814278a13f
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-10-02 23:29:12 +0600

    type conversion

---
+++ typeConversion.js
@@ -0,0 +1,23 @@
+console.log("hello World");
+let a = 10;
+let b = "20";
+console.log(a + b);
+console.log(typeof (a + b));
+
+let c = 10;
+let d = "20";
+console.log(c + d);
+console.log(typeof (c + d));
+
+let e = 10;
+let f = "20";
+console.log(e + f);
+console.log(typeof (e + f));
+
+let g = 10;
+let h = "20";
+console.log(g + h);
+console.log(typeof (g + h));
+
+let i = 10;
+let j = "20";
