9c44ba88a6088d1c9801d703c80039b3fd5513e2
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-10-03 15:01:01 +0600

    typeConversion

--- typeConversion.js
+++ typeConversion.js
@@ -1,23 +1,38 @@
-console.log("hello World");
-let a = 10;
-let b = "20";
-console.log(a + b);
-console.log(typeof (a + b));
+const str = "1000"
+const num = 100
 
-let c = 10;
-let d = "20";
-console.log(c + d);
-console.log(typeof (c + d));
+console.log(str + num)
+console.log(str - num)
+console.log(str * num)
+console.log(str / num)
+console.log(str % num)
 
-let e = 10;
-let f = "20";
-console.log(e + f);
-console.log(typeof (e + f));
+let strToNum = (str) => {
+    let num2 = Number(parseInt(str))
+    return num2
+}
+console.log(strToNum("100") * num)
 
-let g = 10;
-let h = "20";
-console.log(g + h);
-console.log(typeof (g + h));
+const numToString = (num)=>{
+    let string = num.toString();
+    return string
+}
+console.log(strToNum("1000") * numToString(1000))
 
-let i = 10;
-let j = "20";
+ let sum = strToNum("100") + numToString(100) /* this will concate string and number*/
+ let sub = strToNum("100") - numToString(100) /* this will automatically convert string to numbers */
+ let mul = strToNum("100") * numToString(100) /* this will automatically convert string to numbers */
+ let div = strToNum("100") / numToString(100)/* this will automatically convert string to numbers */
+ let mod = strToNum("1001") % numToString(100)/* this will automatically convert string to numbers */
+
+console.log(`
+    if you sum over String and Numbers, you always get the concate value of this two. example:
+    sum (strToNum("100") + numToString(100)) = ${sum} value. 
+    if you sub/multiply/divide/modulus between two string and number values, the string value will automatically change into number value. it this will show the exact result among them.
+    example:
+    sub (strToNum("100") - numToString(100)) = ${sub} value.
+    multiply (strToNum("100") * numToString(100)) = ${mul} value.
+    div (strToNum("100") / numToString(100)) = ${div} value.
+    module (strToNum("1001") % numToString(100)) = ${mod} value.
+    
+`)
