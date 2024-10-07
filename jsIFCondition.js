027bc84cbfe3c2fe1d61e91f98bc5b84cce717fd
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-10-07 08:43:36 +0600

    if condition

---
+++ jsIFCondition.js
@@ -0,0 +1,18 @@
+let a = 10
+let b = 20
+
+if (a>b){
+    console.log(a, ' is greater than ', b  )
+}
+if(a<b){
+    console.log(`${a} is smaller than ${b}`)
+}
+
+let n = 6
+
+if (n % 2 === 1){
+    console.log(n, " is a odd nember ")
+}
+if (n% 2 === 0){
+    console.log(n , "is a Even number")
+}
