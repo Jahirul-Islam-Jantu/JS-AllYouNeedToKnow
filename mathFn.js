5509fcdc71429c47db90cab59a6267f3236d412e
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-10-07 07:59:44 +0600

    math

---
+++ mathFn.js
@@ -0,0 +1,25 @@
+console.log(Math.E) // this is the by default eular's number
+console.log(Math.PI) // by default PI value
+
+
+console.log(Math.min(100, 20 , 30, Math.PI)) // find the minimum number from an array or a list of numbers
+let numberArr = [10, 20, 50]
+let number = 4.535
+
+console.log(Math.abs(number)) // find the absolute value of a number
+console.log(Math.ceil(number)) // this will make your floating number into the next number
+console.log(Math.floor(number)) // this will make your floating number into the same digit without decimal
+console.log(Math.round(number)) // this will make your floating number into the next number if the number in the decimal value is up then 5, otherwise it will return you the same number without decimal value.
+
+console.log(Math.max(0, 100, 20 , 30,  parseInt(numberArr))) // this will return you the max number from an array or a list of numbers.
+
+console.log( Math.round( Math.random() * 100 + 1)) // the random function works here to round the number into integer value and random function returns the random value between 1 - 100
+
+console.log(Math.pow(5, 2)) // this pow function returns the power value of a number
+console.log(Math.pow(3, 3))
+console.log(Math.sqrt(9)) // this sqrt function returns the root value of a number
+console.log(Math.sqrt(25))
+
+
+
+
