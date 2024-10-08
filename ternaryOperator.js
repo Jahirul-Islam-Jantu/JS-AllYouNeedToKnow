a3f9597e55c12bff8afb9e19f7f4312ca08c3645
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-10-08 12:19:43 +0600

    ternary operator

--- ternaryOperator.js
+++ ternaryOperator.js
@@ -3,3 +3,8 @@ const n = 236*68-1*254165467
 console.log(n)
 const result = n%2 === 0? "Even": "Odd"
 console.log(result)
+
+
+// to store data in variable after condition we can use ternary operator.
+// declare variable = condition? true : false
+// if we only have one condition we can use ternary operator, but in multiple condition we have to use normal if else.
