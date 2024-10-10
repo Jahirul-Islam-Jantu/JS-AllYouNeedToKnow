6866d9d7ac506398fb36fb834437ad44d702037d
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-10-10 07:23:42 +0600

    array methods

--- array/methods.js
+++ array/methods.js
@@ -1,1 +1,12 @@
 const arr=[1,2,3,4,5,6];
+
+console.log(arr.join(" | "))  // join method is used for seperate array with anything you provide.
+console.log(arr.fill("Hello")) // fill method will fill the array with anything you want .
+
+let a = [1,2,3,4,5,6];
+let b = Array.from(a); // to copy an array from another array without hampering main data
+
+ b[0] = 55
+
+console.log(b);
+console.log(a);
