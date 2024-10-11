75e5e2dc39930e9f7b8e53ef00af2f23b848265c
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-10-11 08:01:39 +0600

    inner function concept

---
+++ function/innerFn.js
@@ -0,0 +1,12 @@
+function greetings (greet, name){           // this is the main function to take the arguments for greetings . we have inner fn also to do the next process.
+    function getFirstName (){          // this fn is used for name split with the first sentence seperated by comma.
+        if (name){
+            return name.split(" ")[0]
+        }else {
+            return ""
+        }
+    }
+    const greetMessages = greet + " " + getFirstName()
+    console.log(greetMessages)
+}
+greetings("Good Morning", "Jahirul Islam")
