a45b2a50cb6bd40bbccf2c3db432c69abdc7ae9a
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-10-14 08:05:17 +0600

    reduce

---
+++ function/find.js
@@ -0,0 +1,55 @@
+const students = [
+    {
+        id: "std_1",
+        name: "Jahirul Islam",
+        age: 32,
+        email: "jahirulislam@gmail.com"
+    },
+    {
+        id: "std_2",
+        name: "Sahana Islam",
+        age: 35,
+        email: "sahanaislam@gmail.com"
+    },
+    {
+        id: "std_3",
+        name: "Noor - E - Tawhid",
+        age: 5,
+        email: "nooretawhid@gmail.com"
+    },
+    {
+        id: "std_4",
+        name: "Samayrah Islam Zaara",
+        age: .2,
+        email: "samayrah@gmail.com"
+    }
+]
+//
+// const findByValue= students.find(function (value){
+//     return value.name === "Sahana Islam"
+// })
+// console.log(findByValue)
+//
+// const findByIndex = students.findIndex((value)=>{
+//     return value.id === "std_3"
+// })
+// console.log(findByIndex)
+
+function myFind(arr, cb){
+    for (let i = 0; i < arr.length; i++){
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- function/reduce.js
+++ function/reduce.js
@@ -5,8 +5,36 @@ const arr = [1,2,3,4,5,6,7,8,9]
 // }, 300)
 // console.log(newArr)
 
+// const maxVal = arr.reduce((previousValue, currentValue)=>{
+//     return Math.max(previousValue, currentValue)
+// }, 0)
+// console.log(maxVal)
+// const minValue = arr.reduce((acc, cur)=> {
+//     return Math.min (acc, cur)
+// })
+// console.log(minValue)
 
 
+function myReduce (arr, cb , acc){
+    for (let i = 0; i < arr.length; i++){
+        acc = cb(acc, arr[i])
+    }
+    return acc
+}
+
+const max = myReduce(arr, function (prev, cur){
+    return Math.max(prev, cur)
+}, 0)
+console.log(max)
+
+const min = myReduce(arr ,(prev, cur)=>{
+    return Math.min(prev, cur)
+}, arr[0])
+console.log(min)
+const sum = myReduce(arr , (prev, curr)=> {
+    return prev + curr
+}, 0)
+console.log(sum)
 
 
 
