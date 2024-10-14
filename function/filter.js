6266ed0de27d9a57897ee048affdf765203f8f26
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-10-14 06:39:56 +0600

    filter

--- function/filter.js
+++ function/filter.js
@@ -1,16 +1,16 @@
 const arr = [1,35,561,154,43,21,76,94,413,78]
 
-// const newArr =  arr.filter((value, index, arr)=>{
+// const newArr =  arr.filter((value, index, arr)=>{ // filter method also returns new array like map method.
 //     return value < 400
 // })
 // console.log(arr)
 // console.log(newArr)
 
-function myFilter(arr, cb) {
+function myFilter(arr, cb) { //  we take cb and array as argument here to pass the value.
     const newArr = []
     for (let i = 0; i < arr.length; i++) {
-        if (cb(arr[i], i, arr)) {
-            newArr.push(arr[i])
+        if (cb(arr[i], i, arr)) { // this arguments will send as cb parameter
+            newArr.push(arr[i]) // array element to push in new array
         }
     }
     return newArr
---
+++ function/reduce.js
@@ -0,0 +1,19 @@
+const arr = [1,2,3,4,5,6,7,8,9]
+
+// const newArr =  arr.reduce((previousValue, currentValue)=>{
+//     return previousValue + currentValue
+// }, 300)
+// console.log(newArr)
+
+
+
+
+
+
+
+
+
+
+
+
+
