49e748d4bff76ac78fb0f71bd1b9f9882587e96d
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-10-15 22:06:12 +0600

    practice to recursive

--- function/closurewithloop.js
+++ function/closurewithloop.js
@@ -1,5 +1,5 @@
-for (let i = 1; i <= 20; i++){
-    setTimeout(()=>{
-        console.log(i)
-    }, 1000 * i)
+for (let i = 1; i <= 20; i++) {
+  setTimeout(() => {
+    console.log(`${i} Hello World`);
+  }, 1000 * i);
 }
---
+++ scope&closure/sort.js
@@ -0,0 +1,101 @@
+// const persons = [
+//   {
+//     name: "A",
+//     age: 25,
+//   },
+//   {
+//     name: "B",
+//     age: 29,
+//   },
+//   {
+//     name: "C",
+//     age: 21,
+//   },
+//   {
+//     name: "D",
+//     age: 15,
+//   },
+// ];
+
+// persons.sort(function (a, b) {
+//   if (a.age > b.age) {
+//     return -1;
+//   } else if (a.age < b.age) {
+//     return 1;
+//   } else {
+//     return 0;
+//   }
+// });
+// console.log(persons);
+
+// const arr = [
+//   1, 3, 5, 1, 2, 5, 6, 8, 7, 6, 5, 8, 9, -5, -8, -2, -1, 1, 2, 6, 2, 6, 4, 9, 1,
+//   3, 5,
+// ];
+
+// arr.sort((a, b) => {
+//   if (a > b) {
+//     return 1;
+//   } else if (a < b) {
+//     return -1;
+//   } else {
+//     return 0;
+//   }
+// });
+// console.log(arr);
+
+// console.log("Hello Jahir");
+// console.log("Hello Jahir");
+// console.log("Hello Jahir");
+// console.log("Hello Jahir");
+// console.log("Hello Jahir");
+
+// আমি চাচ্ছি একটা নাম ৫০ বার প্রিন্ট করবো কোনো লুপ ছাড়া , বার বার কনসোল ব্যাবহার করা ছাড়া ।
+
+// function forLoop(n) {
+//   if (n === 0) {
+//     return;
+//   }
+
+//   console.log("Hello World");
+
+//   forLoop(n - 1);
+// }
+
+// forLoop(5);
+
+// mr. moon
+// function sayHello(a, b) {
+//   if (b === 0) return;
+//   console.log(a);
+
+//   sayHello(a, b - 1);
+// }
+
+// sayHello("Hello Jahir", 5);
+
+// mr fahim
+
+// const message = "Hello World";
+
+// const count = 5;
+
+// const newMessage = ` ${message}\n`.repeat(count);
+
+// console.log(newMessage);
+
+// Factorial Number
+// function factorialNumber(n) {
+//   if (n === 1) {
+//     return 1;
+//   }
+//   return n * factorialNumber(n - 1);
+// }
+// console.log(factorialNumber(5));
+
+// const number = BigInt(1894151354189466876541658791354887548);
+// console.log(number);
+
+const date = new Date();
+
+console.log(date.toLocaleString());
