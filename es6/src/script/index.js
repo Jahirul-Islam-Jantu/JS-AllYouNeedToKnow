e151eb45c390d939c19399c62479c4410e980b46
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-10-19 22:17:00 +0600

    arrow function

---
+++ es6/src/script/index.js
@@ -0,0 +1,6 @@
+// console.log('Hello World! \n'.repeat(10));
+// Arrow Function
+let add = (a, b) => a + b; //  if we have to return just one line we can make the function like this.
+console.log(add(30, 20))
+let multiply = x => x * x; // if we have only one argument in our function and we have to return in one line we can use our function like this.
+console.log(multiply(3))
