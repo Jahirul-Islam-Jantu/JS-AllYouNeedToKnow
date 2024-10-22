2b49466ad2f8feeef881a0623eab8f7bdc1603fa
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-10-22 22:05:32 +0600

    methods

--- es6/set.js
+++ es6/set.js
@@ -1,25 +1,106 @@
-let set = new Set(["Jahirul Islam", "Jantu", 1,2,3])
+// let set = new Set(["Jahirul Islam", "Jantu", 1,2,3])
+//
+// set.add(4)
+// set.add(5)
+// let ifhas =  set.has(5)
+// console.log(ifhas)
+// let ifthere = set.has(6)
+// console.log(ifthere)
+// let hasthis = set.has("Jahirul Islam")
+// console.log(hasthis)
+//
+// console.log(set)
+// set.delete(5)
+// console.log(set)
+//
+// let iterate = set.keys() // this set data Structure has its own iterator . can be iterete by Set default method like set.keys, set.values, set.entries
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
 
-set.add(4)
-set.add(5)
-let ifhas =  set.has(5)
-console.log(ifhas)
-let ifthere = set.has(6)
-console.log(ifthere)
-let hasthis = set.has("Jahirul Islam")
-console.log(hasthis)
 
-console.log(set)
-set.delete(5)
-console.log(set)
+// const arr = [1,2,3,4,5,6]
+// const obj = {
+//     a: 1,
+//     b: 5,
+//     [Symbol.iterator]: function (){
+//         let current = this.a;
+//         let self = this
+//         return {
+//             next() {
+//                 return{
+//                     done: current >= self.b,
+//                     value: current++
+//                 }
+//             }
+//         }
+//     }
+// }
 
-let iterate = set.keys() // this set data Structure has its own iterator . can be iterete by Set default method like set.keys, set.values, set.entries
-console.log(iterate.next())
-console.log(iterate.next())
-console.log(iterate.next())
-console.log(iterate.next())
-console.log(iterate.next())
-console.log(iterate.next())
-console.log(iterate.next())
+
+// const isIterable = (i)=>{
+//     return typeof i[Symbol.iterator] === 'function'
+// }
+//
+//
+// console.log(isIterable(obj))
+
+
+
+//
+// const iterate = obj[Symbol.iterator]();
+// console.log(iterate.next());
+// console.log(iterate.next());
+// console.log(iterate.next());
+// console.log(iterate.next());
+// console.log(iterate.next());
+
+//
+// const arr = [1,2,3,4,5,6,7,8,9]
+// for(let i = 0; i < arr.length; i++){
+//     if (arr[i] === 5){
+//         break
+//     }
+//         console.log(arr[i])
+// }
+
+
+
+
+
+// console.log(isIterable(arr))
+
+
+// console.log(isIterable(set))
+
+let str = "Hello"
+
+function reverseTheStr (str){
+    return  str.split("").reverse().join("");
+
+}
+console.log(reverseTheStr(str));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
