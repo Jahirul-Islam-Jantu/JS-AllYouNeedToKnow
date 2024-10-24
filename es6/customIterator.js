9534a10eed8395567db70e65df93ff33ba45cf0a
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-10-24 23:17:45 +0600

    factorial

--- es6/customIterator.js
+++ es6/customIterator.js
@@ -21,26 +21,36 @@
 //     }, 1000)
 // }
 
-const arr = [1,2,3,4,5,6]
+// const arr = [1,2,3,4,5,6]
 
-function customIterate (collection){ // this is the blueprint of an es6 custom iterator, which will iterate until the value of i is done false and in a single call it will stop iterate single time then stops execution.this custom iterator will help us to execute the value when we need .
-    let i = 0 // this is the initial value
-    return { // iterator function returns an object
-        next() { // after return we will get a next function and inside next we have to return two parameters.
-            return {
-                done: i >= collection.length, // done parameter will iterate until the collection of an array count
-                value: collection[i++] // value will iterate times ++
+// function customIterate (collection){ // this is the blueprint of an es6 custom iterator, which will iterate until the value of i is done false and in a single call it will stop iterate single time then stops execution.this custom iterator will help us to execute the value when we need .
+//     let i = 0 // this is the initial value
+//     return { // iterator function returns an object
+//         next() { // after return we will get a next function and inside next we have to return two parameters.
+//             return {
+//                 done: i >= collection.length, // done parameter will iterate until the collection of an array count
+//                 value: collection[i++] // value will iterate times ++
 
-            }
-        }
-    }
-}
+//             }
+//         }
+//     }
+// }
 
-const iterate = customIterate(arr)
-console.log(iterate.next()) // we have to call the next function to execute the iteration count each time, until it get it's final value it will return false and execute the number. after every call we will get the next number.
-console.log(iterate.next())
-console.log(iterate.next())
-console.log(iterate.next())
-console.log(iterate.next())
-console.log(iterate.next())
-console.log(iterate.next())
+// const iterate = customIterate(arr)
+// console.log(iterate.next()) // we have to call the next function to execute the iteration count each time, until it get it's final value it will return false and execute the number. after every call we will get the next number.
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+
+// recursive function to find factorial number
+
+const findFactorial = (n) => {
+  if (n < 1) {
+    return 1;
+  }
+  return n * findFactorial(n - 1);
+};
+console.log(findFactorial(4));
